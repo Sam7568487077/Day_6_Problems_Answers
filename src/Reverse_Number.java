acfc99e947ff76af48cc188e378fb7fb6c05ae67
Sam K Varghese <ksam071@gmail.com>
2023-01-03 18:00:00 +0530

    Reverse the number

---
+++ src/Reverse_Number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Reverse_Number {
+    public static void main(String[] args) {
+        int remainder = 0;
+        int reverse = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+
+        int num = sc.nextInt();
+
+        while (num != 0) {
+            remainder = num % 10;
+            reverse = reverse * 10 + remainder;
+            num = num / 10;
+        }
+        System.out.println("The reverse of number is : " + reverse);
+    }
+}
